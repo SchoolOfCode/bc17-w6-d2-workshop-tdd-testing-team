cf5fffb5ca6c32600327e4ceebfbe1424490c608
Paulina G <paulinag@Paulinas-MacBook-Pro.local>
2024-08-06 13:58:41 +0100

    task 2 in progress

--- scrabble-score.test.js
+++ scrabble-score.test.js
@@ -1,11 +1,10 @@
 import { calculateScrabbleScore } from "./scrabble-score";
 import {test, expect} from "vitest"
 
-test('It does not return 1 point', expect(calculateScrabbleScore()).toBe('1'))
+test('It does not return 1 point', expect(calculateScrabbleScore()).toBe(1))
 
 
 
-//  1+1?ebrb:evevbrb
 
 
 
