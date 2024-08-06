904ca9c89f0b96946f40dcf92745c1c45bf43c4a
Hamsa <hamsamuse.hm@gmail.com>
2024-08-06 13:30:07 +0100

    middle of ticket 1

--- scrabble-score.test.js
+++ scrabble-score.test.js
@@ -1,1 +1,14 @@
 import { calculateScrabbleScore } from "./scrabble-score";
+import {test, expect} from "vitest"
+
+test('It does not return 1 point', expect(calculateScrabbleScore()).toBe('1'))
+
+
+
+//  1+1?ebrb:evevbrb
+
+
+
+
+
+
